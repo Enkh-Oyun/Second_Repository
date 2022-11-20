df17dcac412f258a22daebbd14e096e9caa75f7f
Enkh-Oyun <tsendayushenkhoyun@gmail.com>
2022-11-20 21:03:10 +0800

    zasvar

--- 147.py
+++ 147.py
@@ -1,7 +1,3 @@
-a = int(input("Too oruulna uu: "))
-i = 1
-if a > 1:
-    if i % a == 0:
-        print("anhnii too bn")
-    else:
-        print("anhnii too bish bn")
+import sympy
+a = int(input("Hussen toogoo oruulna uu : "))
+print (sympy.isprime(a))
